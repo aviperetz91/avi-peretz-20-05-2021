0318e66db7dd6516eea32aa79e078dcf4556dbd3
Avi <a.peretz33@gmail.com>
2021-05-24 21:57:13 +0300

    WeatherBox component styles

--- src/components/core/WeatherBox/WeatherBox.js
+++ src/components/core/WeatherBox/WeatherBox.js
@@ -1,12 +1,14 @@
+import './WeatherBox.css';
 import React from 'react';
 import moment from 'moment';
 import weatherIcons from '../../../constants/weatherIcons';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { setPath, selectLocation, getCurrentWeather, getForecast } from '../../../store/actions/mainActions';
 
 const WeatherBox = props => {
 
     const { forecast, favorite, history } = props;
+    const { theme } = useSelector(state => state.main);
     const dispatch = useDispatch();
 
     const handleFavoriteClick = () => {
@@ -17,24 +19,55 @@ const WeatherBox = props => {
         history.push("/")
     }
 
+    const styles = {
+        'dark': {
+            backgroundColor: 'rgba(0, 0, 0, 0.7)',
+            color: 'white'
+        },
+        'light': {
+            backgroundColor: 'rgba(255, 255, 255, 0.7)',
+            color: 'black'
+        }
+    }
+
     if (forecast) {
         const fullDate = new Date(forecast.Date);
         const dayString = moment(fullDate).format('dddd');
         return (
-            <div className="border p-2">
-                <h5>{dayString}</h5>
-                <div>{`${forecast.Temperature.Maximum.Value} / ${forecast.Temperature.Minimum.Value}`}</div>
-                <img src={weatherIcons[forecast.Day.Icon]} width="100px" />
-                <div>{forecast.Day.IconPhrase}</div>
+            <div>
+                <div className="day-text-container" style={styles[theme]}>
+                    <h5 className="day-text">{dayString}</h5>
+                </div>
+                <div className="temperature-row">
+                    <div className="temperature-cube" style={styles[theme]}>
+                        <div className="temperature-text">{forecast.Temperature.Maximum.Value}</div>
+                        <div>&#x2109;</div>
+                    </div>
+                    <div className="temperature-cube" style={styles[theme]}>
+                        <div className="temperature-text">{forecast.Temperature.Minimum.Value}</div>
+                        <div>&#x2109;</div>
+                    </div>
+                </div>
+                <div className="weather-icon-container" style={styles[theme]}>
+                    <img src={weatherIcons[forecast.Day.Icon]} width="120px" />
+                    <div className="weather-icon-text">{forecast.Day.IconPhrase}</div>
+                </div>
             </div>
         )
     } else if (favorite) {
         return (
-            <div className="border p-2" style={{ cursor: 'pointer' }} onClick={handleFavoriteClick}>
-                <h5>{favorite.name}</h5>
-                <div>{`${favorite.currentWeather[0].Temperature.Imperial.Value} ${favorite.currentWeather[0].Temperature.Imperial.Unit}`}</div>
-                <img src={weatherIcons[favorite.currentWeather[0].WeatherIcon]} width="120px" />
-                <div>{favorite.currentWeather[0].WeatherText}</div>
+            <div className="box-container" onClick={handleFavoriteClick}>
+                <div className="day-text-container m-0" style={styles[theme]}>
+                    <h5 className="day-text">{favorite.name}</h5>
+                </div>
+                <div className="temperature-rect m-0" style={styles[theme]}>
+                    <div className="temperature-text">{favorite.currentWeather[0].Temperature.Imperial.Value}</div>
+                    <div className="temperature-text">&#x2109;</div>
+                </div>
+                <div className="weather-icon-container" style={styles[theme]}>
+                    <img src={weatherIcons[favorite.currentWeather[0].WeatherIcon]} width="120px" />
+                    <div className="weather-icon-text">{favorite.currentWeather[0].WeatherText}</div>
+                </div>
             </div>
         )
     }
